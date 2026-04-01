b3326098050926e919c9b1df50cdbb39fdc0c8f3
mk <ma_zhengke@outlook.com>
2026-04-01 16:42:30 +0800

    tmp

---
+++ UnityProject/Assets/Scripts/PathTracing/RenderPass/GI/GITemporalResamplingComputePass.cs
@@ -0,0 +1,121 @@
+using mini;
+using Unity.Mathematics;
+using Unity.Profiling;
+using Unity.Profiling.LowLevel;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Rendering.Universal;
+using static PathTracing.ShaderIDs;
+
+namespace PathTracing
+{
+    public class GITemporalResamplingComputePass : ScriptableRenderPass
+    {
+        private const int GroupSize = 8; // RTXDI_SCREEN_SPACE_GROUP_SIZE
+
+        private readonly ComputeShader _computeShader;
+        private Resource _resource;
+        private Settings _settings;
+
+        public GITemporalResamplingComputePass(ComputeShader computeShader)
+        {
+            _computeShader = computeShader;
+        }
+
+        public void Setup(Resource resource, Settings settings)
+        {
+            _resource = resource;
+            _settings = settings;
+        }
+
+        public class Resource
+        {
+            internal GraphicsBuffer ConstantBuffer;
+            internal GraphicsBuffer ResamplingConstantBuffer;
+
+            internal RTHandle Mv;
+
+            internal RTHandle ViewDepth;
+            internal RTHandle DiffuseAlbedo;
+            internal RTHandle SpecularRough;
+            internal RTHandle Normals;
+            internal RTHandle GeoNormals;
+
+            internal RTHandle PrevViewDepth;
+            internal RTHandle PrevDiffuseAlbedo;
+            internal RTHandle PrevSpecularRough;
+            internal RTHandle PrevNormals;
+            internal RTHandle PrevGeoNormals;
+
+            internal RtxdiResources RtxdiResources;
+        }
+
+        public class Settings
+        {
+            internal int2 m_RenderResolution;
+            internal float resolutionScale;
+        }
+
+        class PassData
+        {
+            internal ComputeShader ComputeShader;
+            internal Resource Resource;
+            internal Settings Settings;
+        }
+
+        static void ExecutePass(PassData data, UnsafeGraphContext context)
+        {
+            var natCmd = CommandBufferHelpers.GetNativeCommandBuffer(context.cmd);
+            var marker = new ProfilerMarker(ProfilerCategory.Render, "GITemporalResampling_Compute", MarkerFlags.SampleGPU);
+            natCmd.BeginSample(marker);
+
+            var cs = data.ComputeShader;
+            int kernel = cs.FindKernel("main");
+            var resource = data.Resource;
+            var settings = data.Settings;
+
+            natCmd.SetComputeConstantBufferParam(cs, paramsID, resource.ConstantBuffer, 0, resource.ConstantBuffer.stride);
+            natCmd.SetComputeConstantBufferParam(cs, "g_Const", resource.ResamplingConstantBuffer, 0, resource.ResamplingConstantBuffer.stride);
+
+            natCmd.SetComputeBufferParam(cs, kernel, "u_GIReservoirs", resource.RtxdiResources.GIReservoirBuffer);
+            natCmd.SetComputeBufferParam(cs, kernel, t_NeighborOffsetsID, resource.RtxdiResources.NeighborOffsetsBuffer);
+
+            natCmd.SetComputeTextureParam(cs, kernel, "t_MotionVectors", resource.Mv);
+
+            natCmd.SetComputeTextureParam(cs, kernel, "t_GBufferDepth", resource.ViewDepth);
+            natCmd.SetComputeTextureParam(cs, kernel, "t_GBufferDiffuseAlbedo", resource.DiffuseAlbedo);
+            natCmd.SetComputeTextureParam(cs, kernel, "t_GBufferSpecularRough", resource.SpecularRough);
+            natCmd.SetComputeTextureParam(cs, kernel, "t_GBufferNormals", resource.Normals);
+            natCmd.SetComputeTextureParam(cs, kernel, "t_GBufferGeoNormals", resource.GeoNormals);
+
+            natCmd.SetComputeTextureParam(cs, kernel, "t_PrevGBufferDepth", resource.PrevViewDepth);
+            natCmd.SetComputeTextureParam(cs, kernel, "t_PrevGBufferDiffuseAlbedo", resource.PrevDiffuseAlbedo);
+            natCmd.SetComputeTextureParam(cs, kernel, "t_PrevGBufferSpecularRough", resource.PrevSpecularRough);
+            natCmd.SetComputeTextureParam(cs, kernel, "t_PrevGBufferNormals", resource.PrevNormals);
+            natCmd.SetComputeTextureParam(cs, kernel, "t_PrevGBufferGeoNormals", resource.PrevGeoNormals);
+
+            int rectW = (int)(settings.m_RenderResolution.x * settings.resolutionScale + 0.5f);
+            int rectH = (int)(settings.m_RenderResolution.y * settings.resolutionScale + 0.5f);
+
+            int groupsX = (rectW + GroupSize - 1) / GroupSize;
+            int groupsY = (rectH + GroupSize - 1) / GroupSize;
+
+            natCmd.DispatchCompute(cs, kernel, groupsX, groupsY, 1);
+
+            natCmd.EndSample(marker);
+        }
+
+        public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+        {
+            using var builder = renderGraph.AddUnsafePass<PassData>("GITemporalResampling_Compute", out var passData);
+
+            passData.ComputeShader = _computeShader;
+            passData.Resource = _resource;
+            passData.Settings = _settings;
+
+            builder.AllowPassCulling(false);
+            builder.SetRenderFunc((PassData data, UnsafeGraphContext context) => { ExecutePass(data, context); });
+        }
+    }
+}
---
+++ UnityProject/Assets/Shaders/RayTracing/GI/TemporalResampling.hlsl
@@ -0,0 +1,69 @@
+#pragma max_recursion_depth 1
+
+#include "../RtxdiApplicationBridge/RtxdiApplicationBridge.hlsl"
+
+#ifdef USE_RAY_QUERY
+#define RTXDI_ENABLE_BOILING_FILTER
+#define RTXDI_BOILING_FILTER_GROUP_SIZE RTXDI_SCREEN_SPACE_GROUP_SIZE
+#endif
+
+#include <Assets/Shaders/RTXDI/GI/BoilingFilter.hlsl>
+#include <Assets/Shaders/RTXDI/GI/TemporalResampling.hlsl>
+
+#ifdef USE_RAY_QUERY
+[numthreads(RTXDI_SCREEN_SPACE_GROUP_SIZE, RTXDI_SCREEN_SPACE_GROUP_SIZE, 1)]
+void main(uint2 GlobalIndex : SV_DispatchThreadID, uint2 LocalIndex : SV_GroupThreadID)
+#else
+[shader("raygeneration")]
+void MainRayGenShader()
+#endif
+{
+    #ifndef USE_RAY_QUERY
+    uint2 GlobalIndex = DispatchRaysIndex().xy;
+    uint2 LocalIndex = 0;
+    #endif
+
+    const RTXDI_RuntimeParameters params = g_Const.runtimeParams;
+
+    uint2 pixelPosition = RTXDI_ReservoirPosToPixelPos(GlobalIndex, params.activeCheckerboardField);
+
+    RAB_RandomSamplerState rng = RAB_InitRandomSampler(pixelPosition, 7);
+
+    const RAB_Surface primarySurface = RAB_GetGBufferSurface(pixelPosition, false);
+
+    const uint2 reservoirPosition = RTXDI_PixelPosToReservoirPos(pixelPosition, params.activeCheckerboardField);
+    RTXDI_GIReservoir reservoir = RTXDI_LoadGIReservoir(g_Const.restirGI.reservoirBufferParams, reservoirPosition,
+        g_Const.restirGI.bufferIndices.secondarySurfaceReSTIRDIOutputBufferIndex);
+
+    float3 motionVector = t_MotionVectors[pixelPosition].xyz;
+    motionVector = convertMotionVectorToPixelSpace(g_Const.view, g_Const.prevView, pixelPosition, motionVector);
+
+    if (RAB_IsSurfaceValid(primarySurface))
+    {
+        RTXDI_GITemporalResamplingParameters tParams;
+
+        tParams.screenSpaceMotion = motionVector;
+        tParams.sourceBufferIndex = g_Const.restirGI.bufferIndices.temporalResamplingInputBufferIndex;
+        tParams.maxHistoryLength = g_Const.restirGI.temporalResamplingParams.maxHistoryLength;
+        tParams.biasCorrectionMode = g_Const.restirGI.temporalResamplingParams.temporalBiasCorrectionMode;
+        tParams.depthThreshold = g_Const.restirGI.temporalResamplingParams.depthThreshold;
+        tParams.normalThreshold = g_Const.restirGI.temporalResamplingParams.normalThreshold;
+        tParams.enablePermutationSampling = g_Const.restirGI.temporalResamplingParams.enablePermutationSampling;
+        tParams.enableFallbackSampling = g_Const.restirGI.temporalResamplingParams.enableFallbackSampling;
+        tParams.uniformRandomNumber = g_Const.restirGI.temporalResamplingParams.uniformRandomNumber;
+        tParams.maxReservoirAge = g_Const.restirGI.temporalResamplingParams.maxReservoirAge * (0.5 + RAB_GetNextRandom(rng) * 0.5);
+
+        reservoir = RTXDI_GITemporalResampling(pixelPosition, primarySurface, reservoir, rng,
+            params, g_Const.restirGI.reservoirBufferParams, tParams);
+    }
+
+#ifdef RTXDI_ENABLE_BOILING_FILTER
+    if (g_Const.restirGI.temporalResamplingParams.enableBoilingFilter)
+    {
+        RTXDI_GIBoilingFilter(LocalIndex, g_Const.restirGI.temporalResamplingParams.boilingFilterStrength, reservoir);
+    }
+#endif
+
+    RTXDI_StoreGIReservoir(reservoir, g_Const.restirGI.reservoirBufferParams, reservoirPosition,
+        g_Const.restirGI.bufferIndices.temporalResamplingOutputBufferIndex);
+}
