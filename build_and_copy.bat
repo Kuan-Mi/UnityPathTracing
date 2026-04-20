c4fe6f8dbed7cd411624e01990733de66808f161
mk <ma_zhengke@outlook.com>
2026-04-20 10:58:24 +0800

    pdb

--- build_and_copy.bat
+++ build_and_copy.bat
@@ -6,9 +6,11 @@ echo  NativeRenderPlugin Build and Copy
 echo ============================================================
 echo.
 
-:: Create output directory
+:: Create output directories
 set UNITY_PLUGINS=UnityProject\Packages\top.kuanmi.native-ray-tracing\Plugins\x86_64
+set UNITY_ASSETS_PLUGINS=UnityProject\Assets\Plugins\x86_64
 mkdir "%UNITY_PLUGINS%" 2>nul
+mkdir "%UNITY_ASSETS_PLUGINS%" 2>nul
 
 :: Check if the DLL is locked by Unity
 set DLL_PATH=%UNITY_PLUGINS%\NativeRenderPlugin.dll
@@ -31,19 +33,52 @@ if errorlevel 1 (
 
 :: Copy DLLs to Unity
 echo Copying DLLs to Unity...
-copy /Y "RenderingPlugin\_Build\Debug\NativeRenderPlugin.dll" "%UNITY_PLUGINS%\" >nul
-copy /Y "RenderingPlugin\_Build\Debug\NativeRenderPlugin.pdb" "%UNITY_PLUGINS%\" >nul
+
+:: Project build outputs
+copy /Y "RenderingPlugin\_Build\Debug\NativeRenderPlugin.dll"    "%UNITY_PLUGINS%\" >nul
+copy /Y "RenderingPlugin\_Build\Debug\NativeRenderPlugin.pdb"    "%UNITY_PLUGINS%\" >nul
 copy /Y "RenderingPlugin\_Build\Debug\OMMBakerPlugin.dll"        "%UNITY_PLUGINS%\" >nul
 copy /Y "RenderingPlugin\_Build\Debug\OMMBakerPlugin.pdb"        "%UNITY_PLUGINS%\" >nul
 copy /Y "RenderingPlugin\_Build\Debug\ShaderCompilerPlugin.dll"  "%UNITY_PLUGINS%\" >nul
 copy /Y "RenderingPlugin\_Build\Debug\ShaderCompilerPlugin.pdb"  "%UNITY_PLUGINS%\" >nul
 
+:: Denoiser / PrepareLight -> Assets\Plugins\x86_64
+copy /Y "RenderingPlugin\_Build\Debug\Denoiser.dll"              "%UNITY_ASSETS_PLUGINS%\" >nul
+copy /Y "RenderingPlugin\_Build\Debug\Denoiser.pdb"              "%UNITY_ASSETS_PLUGINS%\" >nul
+copy /Y "RenderingPlugin\_Build\Debug\PrepareLight.dll"          "%UNITY_ASSETS_PLUGINS%\" >nul
+copy /Y "RenderingPlugin\_Build\Debug\PrepareLight.pdb"          "%UNITY_ASSETS_PLUGINS%\" >nul
+
+:: NRD / NRI -> Assets\Plugins\x86_64
+copy /Y "RenderingPlugin\_ExternalBuild\NRD_build\Debug\NRD.dll" "%UNITY_ASSETS_PLUGINS%\" >nul
+copy /Y "RenderingPlugin\_ExternalBuild\NRD_build\Debug\NRD.pdb" "%UNITY_ASSETS_PLUGINS%\" >nul
+copy /Y "RenderingPlugin\_ExternalBuild\NRI_build\Debug\NRI.dll" "%UNITY_ASSETS_PLUGINS%\" >nul
+copy /Y "RenderingPlugin\_ExternalBuild\NRI_build\Debug\NRI.pdb" "%UNITY_ASSETS_PLUGINS%\" >nul
+
+:: DLSS / DLSS-D -> Assets\Plugins\x86_64
+copy /Y "RenderingPlugin\_ExternalBuild\NRI_build\Debug\nvngx_dlss.dll"  "%UNITY_ASSETS_PLUGINS%\" >nul
+copy /Y "RenderingPlugin\_ExternalBuild\NRI_build\Debug\nvngx_dlssd.dll" "%UNITY_ASSETS_PLUGINS%\" >nul
+
+
+:: DXC (dxcompiler / dxil)
+copy /Y "RenderingPlugin\_deps\dxc-nuget\build\native\bin\x64\dxcompiler.dll" "%UNITY_PLUGINS%\" >nul
+copy /Y "RenderingPlugin\_deps\dxc-nuget\build\native\bin\x64\dxil.dll"       "%UNITY_PLUGINS%\" >nul
+
 echo.
 echo ============================================================
 echo  Build and copy successful!
-echo  Output: %UNITY_PLUGINS%\NativeRenderPlugin.dll
-echo           %UNITY_PLUGINS%\OMMBakerPlugin.dll
-echo           %UNITY_PLUGINS%\ShaderCompilerPlugin.dll
+echo  Package Plugins: %UNITY_PLUGINS%\
+echo    NativeRenderPlugin.dll
+echo    OMMBakerPlugin.dll
+echo    ShaderCompilerPlugin.dll
+echo    dxcompiler.dll
+echo    dxil.dll
+echo  Assets Plugins:  %UNITY_ASSETS_PLUGINS%\
+echo    Denoiser.dll
+echo    PrepareLight.dll
+echo    NRD.dll
+echo    NRI.dll
+echo    nvngx_dlss.dll
+echo    nvngx_dlssd.dll
 echo ============================================================
 echo.
 pause
