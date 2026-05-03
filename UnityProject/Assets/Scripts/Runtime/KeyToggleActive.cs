6bff67f4bf7486c76e04a1cc860db01a4f1f4dde
MK <Ma_ZhenKe@outlook.com>
2026-05-03 18:15:21 +0800

    toggle

---
+++ UnityProject/Assets/Scripts/Runtime/KeyToggleActive.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Runtime
+{
+    public class KeyToggleActive : MonoBehaviour
+    {
+        [SerializeField]
+        private KeyCode _key = KeyCode.Space;
+
+        public GameObject target;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(_key))
+            {
+                target.SetActive(!target.activeSelf);
+            }
+        }
+    }
+}
