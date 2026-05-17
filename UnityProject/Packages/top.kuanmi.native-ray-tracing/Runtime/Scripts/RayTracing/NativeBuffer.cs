4acde896be4c7c426c9b6d72b7465c838080f362
MK <Ma_ZhenKe@outlook.com>
2026-05-17 11:29:33 +0800

    NativeBuffer

--- UnityProject/Packages/top.kuanmi.native-ray-tracing/Runtime/Scripts/RayTracing/NativeBuffer.cs
+++ UnityProject/Packages/top.kuanmi.native-ray-tracing/Runtime/Scripts/RayTracing/NativeBuffer.cs
@@ -11,27 +11,27 @@ namespace NativeRender
 {
     public sealed class NativeBuffer : IDisposable
     {
-        public ulong Handle { get; private set; }
-        private bool _disposed;
+        public  ulong Handle { get; private set; }
+        private bool  _disposed;
 
-        private const int BufferCount = 3; // 三缓冲
-        private int _bufferIndex = 0;
+        private const    int BufferCount  = 3; // 三缓冲
+        private          int _bufferIndex = 0;
         private readonly int _singleFrameSize;
-        
+
         // 1. 数据缓冲区数组
-        private NativeArray<byte>[] _frameDataArray; 
+        private NativeArray<byte>[] _frameDataArray;
+
         // 2. 参数缓冲区数组 (用于传递给 Native)
         private NativeArray<UploadParams>[] _paramsArray;
 
-        private bool _hasUpdateThisFrame;
-        private int _lastUpdateFrame = -1;
         private readonly InternalUploadPass _internalPass;
 
         [StructLayout(LayoutKind.Sequential)]
-        struct UploadParams {
-            public ulong BufferHandle;
+        struct UploadParams
+        {
+            public ulong  BufferHandle;
             public IntPtr SourceDataPtr;
-            public uint Size;
+            public uint   Size;
         }
 
         public NativeBuffer(int sizeInBytes)
@@ -39,53 +39,44 @@ namespace NativeRender
             if (sizeInBytes <= 0) throw new ArgumentOutOfRangeException(nameof(sizeInBytes));
 
             _singleFrameSize = (sizeInBytes + 255) & ~255;
-            Handle = NativeRenderPlugin.NR_CreateNativeBuffer((uint)_singleFrameSize);
+            Handle           = NativeRenderPlugin.NR_CreateNativeBuffer((uint)_singleFrameSize);
 
             _frameDataArray = new NativeArray<byte>[BufferCount];
-            _paramsArray = new NativeArray<UploadParams>[BufferCount];
+            _paramsArray    = new NativeArray<UploadParams>[BufferCount];
 
             for (int i = 0; i < BufferCount; i++)
             {
                 _frameDataArray[i] = new NativeArray<byte>(_singleFrameSize, Allocator.Persistent);
-                _paramsArray[i] = new NativeArray<UploadParams>(1, Allocator.Persistent);
+                _paramsArray[i]    = new NativeArray<UploadParams>(1, Allocator.Persistent);
             }
 
             _internalPass = new InternalUploadPass(this);
         }
 
+        // todo 目前要手动确保每帧只调用一次
         public unsafe void Upload<T>(ScriptableRenderer renderer, T data) where T : unmanaged
         {
             if (_disposed) return;
 
-            // 每一帧第一次调用时切换缓冲区索引
-            if (_lastUpdateFrame != Time.frameCount)
-            {
-                _bufferIndex = (_bufferIndex + 1) % BufferCount;
-                _lastUpdateFrame = Time.frameCount;
-                _hasUpdateThisFrame = false;
-            }
-
             // 拷贝数据到当前帧对应的缓冲区
-            var currentData = _frameDataArray[_bufferIndex];
-            void* srcPtr = UnsafeUtility.AddressOf(ref data);
-            void* dstPtr = currentData.GetUnsafePtr();
+            var   currentData = _frameDataArray[_bufferIndex];
+            void* srcPtr      = UnsafeUtility.AddressOf(ref data);
+            void* dstPtr      = currentData.GetUnsafePtr();
             UnsafeUtility.MemCpy(dstPtr, srcPtr, Math.Min(sizeof(T), _singleFrameSize));
 
-            if (!_hasUpdateThisFrame)
-            {
-                renderer.EnqueuePass(_internalPass);
-                _hasUpdateThisFrame = true;
-            }
+            _internalPass.Setup(_bufferIndex);
+            renderer.EnqueuePass(_internalPass);
         }
 
-        private unsafe void Execute(UnsafeCommandBuffer cmd)
+        private unsafe void Execute(UnsafeCommandBuffer cmd, int index)
         {
-            // 准备这一帧的参数
-            var currentParams = _paramsArray[_bufferIndex];
-            currentParams[0] = new UploadParams {
-                BufferHandle = Handle,
-                SourceDataPtr = (IntPtr)_frameDataArray[_bufferIndex].GetUnsafePtr(),
-                Size = (uint)_singleFrameSize
+            var currentParams = _paramsArray[index];
+
+            currentParams[0] = new UploadParams
+            {
+                BufferHandle  = Handle,
+                SourceDataPtr = (IntPtr)_frameDataArray[index].GetUnsafePtr(),
+                Size          = (uint)_singleFrameSize
             };
 
             // 获取参数的固定地址传给 Native
@@ -99,20 +90,34 @@ namespace NativeRender
         private class InternalUploadPass : ScriptableRenderPass
         {
             private readonly NativeBuffer _owner;
-            public InternalUploadPass(NativeBuffer owner) {
-                _owner = owner;
+            private          int          _recordedIndex;
+
+            public void Setup(int index)
+            {
+                _recordedIndex = index;
+            }
+
+            public InternalUploadPass(NativeBuffer owner)
+            {
+                _owner          = owner;
                 renderPassEvent = RenderPassEvent.BeforeRendering;
             }
 
             public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
             {
-                using var builder = renderGraph.AddUnsafePass<EmptyPassData>("NativeBufferUpload", out _);
+                using var builder = renderGraph.AddUnsafePass<UploadPassData>("NativeBufferUpload", out var passData);
+
+
+                passData.BufferIndex = _recordedIndex;
+
                 builder.AllowPassCulling(false);
-                builder.SetRenderFunc((EmptyPassData _, UnsafeGraphContext context) => {
-                    _owner.Execute(context.cmd);
-                });
+                builder.SetRenderFunc((UploadPassData data, UnsafeGraphContext context) => { _owner.Execute(context.cmd, data.BufferIndex); });
             }
-            class EmptyPassData { }
+
+            class UploadPassData
+            {
+                public int BufferIndex;
+            }
         }
 
         public void Dispose()
@@ -123,6 +128,7 @@ namespace NativeRender
                 if (_frameDataArray[i].IsCreated) _frameDataArray[i].Dispose();
                 if (_paramsArray[i].IsCreated) _paramsArray[i].Dispose();
             }
+
             if (Handle != 0) NativeRenderPlugin.NR_DestroyNativeBuffer(Handle);
             _disposed = true;
         }
