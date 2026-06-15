eb9ee9276390412d5791a4ec39212bf4039df8a0
MK <Ma_ZhenKe@outlook.com>
2026-06-15 22:02:04 +0800

    Visual Studio version

--- build.bat
+++ build.bat
@@ -28,6 +28,27 @@ echo.
 
 set BUILD_DIR=RenderingPlugin\_Build
 
+:: Detect the installed Visual Studio version via vswhere (ships with VS 2017+).
+set VSWHERE="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if not exist %VSWHERE% (
+    echo [ERROR] vswhere.exe not found. Is Visual Studio 2017 or newer installed?
+    exit /b 1
+)
+
+set VS_MAJOR=
+for /f "tokens=1 delims=." %%v in ('%VSWHERE% -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationVersion') do set VS_MAJOR=%%v
+
+if "%VS_MAJOR%"=="17" set CMAKE_GENERATOR=Visual Studio 17 2022
+if "%VS_MAJOR%"=="16" set CMAKE_GENERATOR=Visual Studio 16 2019
+if "%VS_MAJOR%"=="15" set CMAKE_GENERATOR=Visual Studio 15 2017
+
+if "%CMAKE_GENERATOR%"=="" (
+    echo [ERROR] No supported Visual Studio with C++ tools found ^(detected version: "%VS_MAJOR%"^).
+    exit /b 1
+)
+echo [OK] Using generator: %CMAKE_GENERATOR%
+echo.
+
 :: Configure CMake (only when cache and project files are both present)
 if not exist "%BUILD_DIR%\CMakeCache.txt" goto :configure
 if not exist "%BUILD_DIR%\ALL_BUILD.vcxproj" goto :configure
@@ -35,7 +56,7 @@ goto :build
 
 :configure
 echo Configuring CMake...
-cmake -S RenderingPlugin -B "%BUILD_DIR%" -G "Visual Studio 17 2022" -A x64 -T host=x64 -DNR_SKIP_UNITY_COPY=ON
+cmake -S RenderingPlugin -B "%BUILD_DIR%" -G "%CMAKE_GENERATOR%" -A x64 -T host=x64 -DNR_SKIP_UNITY_COPY=ON
 if errorlevel 1 (
     echo [ERROR] CMake configuration failed.
     exit /b 1
